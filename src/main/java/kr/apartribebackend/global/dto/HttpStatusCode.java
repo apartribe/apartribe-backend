4817e9dd24593804a0e7bf50500a5719eeb3aedc
Revi1337 <david122123@gmail.com>
2023-09-14 08:29:08 +0900

    feat: 공통된 글로벌 익셉션 처리

---
+++ src/main/java/kr/apartribebackend/global/dto/HttpStatusCode.java
@@ -0,0 +1,11 @@
+package kr.apartribebackend.global.dto;
+
+
+public interface HttpStatusCode {
+
+    int BAD_REQUEST = 400;
+    int NOT_FOUND = 404;
+    int UNAUTHORIZED = 401;
+    int FORBIDDEN = 403;
+
+}
