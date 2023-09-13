b3f81904e202af1aba05ec830a659a2eb6ea73d5
Revi1337 <david122123@gmail.com>
2023-09-13 17:44:56 +0900

    feat: 멤버 도메인 및 dto response 작성

---
+++ src/main/java/kr/apartribebackend/member/domain/dto/MemberResponse.java
@@ -0,0 +1,20 @@
+package kr.apartribebackend.member.domain.dto;
+
+
+public record MemberResponse(
+        Long id,
+        String email,
+        String name,
+        String nickname
+) {
+
+    public static MemberResponse from(MemberDto memberDto) {
+        return new MemberResponse(
+                memberDto.getId(),
+                memberDto.getEmail(),
+                memberDto.getName(),
+                memberDto.getNickname()
+        );
+    }
+
+}
