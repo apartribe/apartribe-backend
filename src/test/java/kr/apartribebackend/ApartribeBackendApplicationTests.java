971d1df6ab8bb5107e923d8de9e3d527f5e36cc6
Revi1337 <david122123@gmail.com>
2023-09-13 12:58:49 +0900

    init: 프로젝트 초기화

---
+++ src/test/java/kr/apartribebackend/ApartribeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.apartribebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartribeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
