5210dc39e54bc0c1d1d88cdffdef4a3349234870
Revi1337 <david122123@gmail.com>
2023-09-14 21:26:43 +0900

    feat: 미처 추가하지못한 이메일토큰 관련 익셉션 추가

---
+++ src/main/java/kr/apartribebackend/token/email/exception/NotAuthenticatedEmailException.java
@@ -0,0 +1,10 @@
+package kr.apartribebackend.token.email.exception;
+
+import kr.apartribebackend.global.exception.RootException;
+
+public class NotAuthenticatedEmailException extends RootException {
+    
+    public NotAuthenticatedEmailException() {
+        super("인증되지않은 이메일입니다.");
+    }
+}
