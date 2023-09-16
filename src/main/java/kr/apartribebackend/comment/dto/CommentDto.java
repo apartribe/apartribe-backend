0affd3aa13a99f29bd2cefa87b7c91e835b3d641
Revi1337 <david122123@gmail.com>
2023-09-16 16:00:28 +0900

    feat: 댓글작성 요청 request, 댓글 dto 작성

---
+++ src/main/java/kr/apartribebackend/comment/dto/CommentDto.java
@@ -0,0 +1,48 @@
+package kr.apartribebackend.comment.dto;
+
+import kr.apartribebackend.article.domain.Article;
+import kr.apartribebackend.comment.domain.Comment;
+import kr.apartribebackend.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CommentDto {
+
+    private Long id;
+    private String content;
+    private int liked;
+    private Article article;
+
+    @Builder
+    private CommentDto(Long id,
+                       String content,
+                       int liked,
+                       Article article) {
+        this.id = id;
+        this.content = content;
+        this.liked = liked;
+        this.article = article;
+    }
+
+    public static CommentDto from(Comment comment) {
+        return CommentDto.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .liked(comment.getLiked())
+                .article(comment.getArticle())
+                .build();
+    }
+
+    public Comment toEntity(Member member, Article article) {
+        return Comment.builder()
+                .id(id)
+                .content(content)
+                .liked(liked)
+                .article(article)
+                .member(member)
+                .build();
+
+    }
+
+}
