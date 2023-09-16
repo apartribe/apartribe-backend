392244dc933e7229dce0356fb1d3623a6eb22634
Revi1337 <david122123@gmail.com>
2023-09-16 18:54:55 +0900

    feat: 지난주 베스트댓글 쿼리 작성

--- src/main/java/kr/apartribebackend/comment/repository/CommentRepository.java
+++ src/main/java/kr/apartribebackend/comment/repository/CommentRepository.java
@@ -3,7 +3,8 @@ package kr.apartribebackend.comment.repository;
 import kr.apartribebackend.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends
+        JpaRepository<Comment, Long>, CustomCommentRepository {
 
 
 }
