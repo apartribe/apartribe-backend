4e2b4d1e5dc8d992411df35b2ee53107ae02a239
Revi1337 <david122123@gmail.com>
2023-09-18 20:34:23 +0900

    feat: json 로그인을 위한 security bean 작성

--- src/main/java/kr/apartribebackend/global/config/SecurityConfig.java
+++ src/main/java/kr/apartribebackend/global/config/SecurityConfig.java
@@ -1,7 +1,19 @@
 package kr.apartribebackend.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.apartribebackend.global.filter.DelegatingRedirectUrlFilter;
+import kr.apartribebackend.global.filter.JsonLoginAuthenticationFilter;
+import kr.apartribebackend.global.handler.JsonLoginSuccessHandler;
+import kr.apartribebackend.global.provider.JsonLoginAuthenticationProvider;
+import kr.apartribebackend.global.service.JsonLoginUserDetailsService;
+import kr.apartribebackend.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,25 +22,63 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+@RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
+
+    private final MemberRepository memberRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .anyRequest().permitAll())
+                .addFilterAfter(jsonLoginAuthenticationFilter(), DelegatingRedirectUrlFilter.class)
                 .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public JsonLoginAuthenticationFilter jsonLoginAuthenticationFilter() {
+        final JsonLoginAuthenticationFilter jsonLoginAuthenticationFilter =
+                new JsonLoginAuthenticationFilter(objectMapper);
+        jsonLoginAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        jsonLoginAuthenticationFilter.setAuthenticationSuccessHandler(JsonLoginSuccessHandler());
+        return jsonLoginAuthenticationFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(authenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new JsonLoginAuthenticationProvider(userDetailsService(), passwordEncoder());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new JsonLoginUserDetailsService(memberRepository);
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler JsonLoginSuccessHandler() {
+        return new JsonLoginSuccessHandler();
     }
 
 }
