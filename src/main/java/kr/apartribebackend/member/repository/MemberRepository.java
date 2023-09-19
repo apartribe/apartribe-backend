b7e42f20fe8805b74d39fc590150541b58bbe151
Revi1337 <david122123@gmail.com>
2023-09-19 10:31:34 +0900

    feat: 레포지토리에 nickname 으로 찾는 쿼리추가

--- src/main/java/kr/apartribebackend/member/repository/MemberRepository.java
+++ src/main/java/kr/apartribebackend/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package kr.apartribebackend.member.repository;
 
 import kr.apartribebackend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,8 +12,15 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String Email);
 
+    Optional<Member> findByNickname(String nickname);
+
     boolean existsByEmail(String Email);
 
     boolean existsByNickname(String nickname);
+
+    Optional<Member> findByEmailAndName(String email, String name);
+
+    @Query(value = "select m from Member as m inner join m.refreshToken as t where m.email = :email")
+    Optional<Member> findRefreshTokenByEmail(@Param("email") String email);
 
 }
