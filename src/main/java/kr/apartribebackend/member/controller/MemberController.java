8bd5d4363ea5db9e9862c21026d2e44db1e5ecd1
Revi1337 <david122123@gmail.com>
2023-09-19 23:14:35 +0900

    feat: 멤버의 패스워드, 닉네임 변경 기능 추가

--- src/main/java/kr/apartribebackend/member/controller/MemberController.java
+++ src/main/java/kr/apartribebackend/member/controller/MemberController.java
@@ -4,15 +4,14 @@ import jakarta.validation.Valid;
 import kr.apartribebackend.global.dto.APIResponse;
 import kr.apartribebackend.global.dto.PageResponse;
 import kr.apartribebackend.global.exception.PasswordNotEqualException;
-import kr.apartribebackend.member.dto.MemberDto;
-import kr.apartribebackend.member.dto.MemberResponse;
-import kr.apartribebackend.member.dto.MemberUpdateReq;
-import kr.apartribebackend.member.dto.NicknameIsValidResponse;
+import kr.apartribebackend.member.dto.*;
+import kr.apartribebackend.member.principal.AuthenticatedMember;
 import kr.apartribebackend.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -29,7 +28,7 @@ public class MemberController {
         return apiResponse;
     }
 
-    @GetMapping("/api/members")
+    @GetMapping("/api/member")
     public APIResponse<PageResponse<MemberResponse>> findAllMembers(@PageableDefault final Pageable pageable) {
         final Page<MemberResponse> pageMembers = memberService.findAllMembers(pageable).map(MemberResponse::from);
         final PageResponse<MemberResponse> pageResponse = PageResponse.from(pageMembers);
@@ -37,9 +36,11 @@ public class MemberController {
         return apiResponse;
     }
 
+    // TODO 아직 수정 전이며, 보류
     @PutMapping("/api/member/update")
     public void updateSingleMember(
-            @RequestHeader(value = "EMAIL", required = false) final String email,                                   // TODO 나중에 토큰값에서 꺼내오는것으로 변경해야 한다.
+            @RequestHeader(value = "EMAIL", required = false) final String email,
+            @AuthenticationPrincipal final AuthenticatedMember authenticatedMember,
             @RequestBody @Valid final MemberUpdateReq memberUpdateReq
     ) {
         if (!memberUpdateReq.password().equals(memberUpdateReq.passwordConfirm()))
@@ -49,8 +50,31 @@ public class MemberController {
         memberService.updateSingleMember(memberDto, email);
     }
 
+    @PutMapping("/api/member/update/password")
+    public void updateSingleMemberPassword(
+            @AuthenticationPrincipal final AuthenticatedMember authenticatedMember,
+            @Valid @RequestBody final MemberChangePasswordReq memberChangePasswordReq
+    ) {
+        if (!memberChangePasswordReq.newPassword().equals(memberChangePasswordReq.passwordConfirm()))
+            throw new PasswordNotEqualException();
+
+        memberService.updateSingleMemberPassword(authenticatedMember, memberChangePasswordReq);
+    }
+
+    @PutMapping("/api/member/update/nickname")
+    public void updateSingleMemberNickname(
+            @AuthenticationPrincipal final AuthenticatedMember authenticatedMember,
+            @Valid @RequestBody final MemberChangeNicknameReq memberChangeNicknameReq
+    ) {
+        memberService.updateSingleMemberNickname(authenticatedMember, memberChangeNicknameReq.nickname());
+    }
+
+    // TODO 아직 수정 전이며, 보류
     @DeleteMapping("/api/member/delete")
-    public void deleteSingleUser(@RequestHeader(value = "EMAIL", required = false) final String email) {            // TODO 나중에 토큰값에서 꺼내오는것으로 변경해야 한다.
+    public void deleteSingleUser(
+            @RequestHeader(value = "EMAIL", required = false) final String email,
+            @AuthenticationPrincipal final AuthenticatedMember authenticatedMember
+    ) {
         memberService.deleteSingleUser(email);
     }
 
