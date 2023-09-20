e2b8d65aba1e5dafe82c44d2038359e9054cc486
Revi1337 <david122123@gmail.com>
2023-09-20 20:38:25 +0900

    feat: 파일업로드 API 및 서비스 작성

--- src/main/java/kr/apartribebackend/article/service/ArticleService.java
+++ src/main/java/kr/apartribebackend/article/service/ArticleService.java
@@ -6,6 +6,8 @@ import kr.apartribebackend.article.dto.*;
 import kr.apartribebackend.article.exception.ArticleNotFoundException;
 import kr.apartribebackend.article.exception.CannotReflectLikeToArticleException;
 import kr.apartribebackend.article.repository.ArticleRepository;
+import kr.apartribebackend.attachment.domain.Attachment;
+import kr.apartribebackend.attachment.service.AttachmentService;
 import kr.apartribebackend.member.domain.Member;
 import kr.apartribebackend.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -23,6 +27,8 @@ import java.util.List;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+
+    private final AttachmentService attachmentService;
 
     public Page<ArticleDto> findMultipleArticlesByCategory(final Category category, final Pageable pageable) {
         if (category.equals(Category.ALL))
@@ -59,10 +65,22 @@ public class ArticleService {
     }
 
     @Transactional
-    public void appendArticle(final ArticleDto articleDto, final MemberDto memberDto) {
+    public Article appendArticle(final ArticleDto articleDto, final MemberDto memberDto) {
         final Member member = memberDto.toEntity();
         final Article article = articleDto.toEntity(member);
-        articleRepository.save(article);
+        return articleRepository.save(article);
+    }
+
+    @Transactional
+    public void appendArticle(final ArticleDto articleDto,
+                              final MemberDto memberDto,
+                              final List<MultipartFile> file) throws IOException {
+        Article article = appendArticle(articleDto, memberDto);
+        List<Attachment> attachments = attachmentService.saveFiles(file);
+        for (Attachment attachment : attachments) {
+            attachment.registArticle(article);
+        }
+        attachmentService.saveAttachments(attachments);
     }
 
     public List<ArticleInCommunityRes> searchArticleInCommunity(String title) {
