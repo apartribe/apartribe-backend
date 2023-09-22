34edb7d4b4045e7070a94f12b894f0a737e900eb
Revi1337 <david122123@gmail.com>
2023-09-22 11:16:46 +0900

    feat: 상속관계 builder 를 위해 기본 생성자 추가

--- src/main/java/kr/apartribebackend/global/domain/TimeBaseEntity.java
+++ src/main/java/kr/apartribebackend/global/domain/TimeBaseEntity.java
@@ -4,7 +4,10 @@ package kr.apartribebackend.global.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +15,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass @Getter
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeBaseEntity {
