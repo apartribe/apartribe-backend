151dad3a9e4c95b80e44f3bbb5cfabb99364e70d
Revi1337 <david122123@gmail.com>
2023-09-23 12:48:27 +0900

    refactor: custom 카테고리 converter 수정

---
+++ src/main/java/kr/apartribebackend/article/domain/CategoryCondition.java
@@ -0,0 +1,11 @@
+package kr.apartribebackend.article.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter @AllArgsConstructor
+public class CategoryCondition {
+
+    private String name;
+
+}
