038dc458bf2720b7747f59dd827d2ab58ab822e9
Revi1337 <david122123@gmail.com>
2023-09-23 21:45:55 +0900

    refactor: 게시글 dto 수정

--- src/main/java/kr/apartribebackend/article/dto/ArticleResponse.java
+++ src/main/java/kr/apartribebackend/article/dto/ArticleResponse.java
@@ -1,23 +1,76 @@
 package kr.apartribebackend.article.dto;
 
 
+
+import kr.apartribebackend.article.domain.Article;
+
+import java.time.LocalDateTime;
+
 public record ArticleResponse(
         Long id,
-        String category,
+//        String category,
         int liked,
         int saw,
         String title,
-        String content
+        String content,
+        LocalDateTime createdAt,
+        String createdBy
 ) {
+
+    public static ArticleResponse from(Article article) {
+        return new ArticleResponse(
+                article.getId(),
+//                article.getCategory().getName(),
+                article.getLiked(),
+                article.getSaw(),
+                article.getTitle(),
+                article.getContent(),
+                article.getCreatedAt(),
+                article.getCreatedBy()
+        );
+    }
 
     public static ArticleResponse from(ArticleDto articleDto) {
         return new ArticleResponse(
                 articleDto.getId(),
-                articleDto.getCategory().getName(),
+//                articleDto.getCategory().getName(),
                 articleDto.getLiked(),
                 articleDto.getSaw(),
                 articleDto.getTitle(),
-                articleDto.getContent()
+                articleDto.getContent(),
+                articleDto.getCreatedAt(),
+                articleDto.getCreatedBy()
         );
     }
 }
+
+
+//package kr.apartribebackend.article.dto;
+//
+//
+//import java.time.LocalDateTime;
+//
+//public record ArticleResponse(
+//        Long id,
+//        String category,
+//        int liked,
+//        int saw,
+//        String title,
+//        String content,
+//        LocalDateTime createdAt,
+//        String createdBy
+//) {
+//
+//    public static ArticleResponse from(ArticleDto articleDto) {
+//        return new ArticleResponse(
+//                articleDto.getId(),
+//                articleDto.getCategory().getName(),
+//                articleDto.getLiked(),
+//                articleDto.getSaw(),
+//                articleDto.getTitle(),
+//                articleDto.getContent(),
+//                articleDto.getCreatedAt(),
+//                articleDto.getCreatedBy()
+//        );
+//    }
+//}
