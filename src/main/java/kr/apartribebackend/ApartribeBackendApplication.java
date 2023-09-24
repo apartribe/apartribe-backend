c21b6f94116af9978e049270b35cb0843f04d90c
Revi1337 <david122123@gmail.com>
2023-09-24 13:44:18 +0900

    feat: 카테고리 초기 데이터 생성

--- src/main/java/kr/apartribebackend/ApartribeBackendApplication.java
+++ src/main/java/kr/apartribebackend/ApartribeBackendApplication.java
@@ -1,13 +1,23 @@
 package kr.apartribebackend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
+
+@Slf4j
 @SpringBootApplication
 public class ApartribeBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApartribeBackendApplication.class, args);
+	}
+
+	@EventListener(value = ApplicationReadyEvent.class)
+	public void eventListener() {
+		log.info("ApplicationReadyEvent called() -3-");
 	}
 
 }
