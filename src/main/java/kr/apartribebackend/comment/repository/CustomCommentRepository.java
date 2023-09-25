8ad4d524befcff6cde22779a812054658df0e004
Revi1337 <david122123@gmail.com>
2023-09-25 20:02:39 +0900

    feat: 대댓글 레포지토리 코드 추가

--- src/main/java/kr/apartribebackend/comment/repository/CustomCommentRepository.java
+++ src/main/java/kr/apartribebackend/comment/repository/CustomCommentRepository.java
@@ -2,11 +2,16 @@ package kr.apartribebackend.comment.repository;
 
 
 import kr.apartribebackend.comment.dto.BestCommentResponse;
+import kr.apartribebackend.comment.dto.CommentRes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CustomCommentRepository {
 
     List<BestCommentResponse> bestCommentRankViaLastWeek();
+
+    Page<CommentRes> findCommentsByBoardId(final Long boardId, final Pageable pageable);
 
 }
