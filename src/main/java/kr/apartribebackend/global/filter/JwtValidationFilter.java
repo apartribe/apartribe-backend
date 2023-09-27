6d33bd6a9a3e9387fa5b3349fc85d1302540855b
Revi1337 <david122123@gmail.com>
2023-09-27 22:48:04 +0900

    feat: 리프레시토큰 검증 로직과 RTR 구현

--- src/main/java/kr/apartribebackend/global/filter/JwtValidationFilter.java
+++ src/main/java/kr/apartribebackend/global/filter/JwtValidationFilter.java
@@ -1,12 +1,16 @@
 package kr.apartribebackend.global.filter;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.apartribebackend.global.dto.ReIssueTokenReq;
+import kr.apartribebackend.global.dto.TokenResponse;
+import kr.apartribebackend.global.exception.NotExistsRefreshTokenException;
 import kr.apartribebackend.global.service.JwtService;
 import kr.apartribebackend.member.domain.Member;
 import kr.apartribebackend.member.dto.MemberDto;
@@ -16,12 +20,15 @@ import kr.apartribebackend.token.refresh.domain.RefreshToken;
 import kr.apartribebackend.token.refresh.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -40,6 +47,8 @@ public class JwtValidationFilter extends OncePerRequestFilter {
 
     private final RefreshTokenRepository refreshTokenRepository;
 
+    private final ObjectMapper objectMapper;
+
     private String reIssuedTokenPath = "/api/reissue/token";
 
     private Set<String> filterExcludePath = Set.of("/api/auth");
@@ -52,7 +61,6 @@ public class JwtValidationFilter extends OncePerRequestFilter {
         this.filterExcludePath = filterExcludePath;
     }
 
-    // TODO 전체적인 리팩토링 필요
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
@@ -62,30 +70,33 @@ public class JwtValidationFilter extends OncePerRequestFilter {
             return;
         }
         if (request.getRequestURI().equals(reIssuedTokenPath)) {
-            final String refreshCookieValue = extractRefreshCookieValue(request);
-            if (refreshCookieValue == null)
+            final String reIssueTokenString = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+            final ReIssueTokenReq reIssueTokenReq = objectMapper.readValue(reIssueTokenString, ReIssueTokenReq.class);
+            if (reIssueTokenReq == null)
                 return;
 
-            final String subjectNickname = jwtService.extractClaim(
-                    refreshCookieValue, JwtService.TokenType.REFRESH, Claims::getSubject);
-            Member member = memberRepository.findByNickname(subjectNickname).orElse(null);
-            if (member == null)
-                return;
+            final Claims extractedAllClaims = jwtService.extractAllClaims(
+                    reIssueTokenReq.refreshToken(), JwtService.TokenType.REFRESH);
+            final String subjectNickname = extractedAllClaims.getSubject();
+            final String tokenType = (String) extractedAllClaims.get("type");
+
+            final Member member = memberRepository.findByNickname(subjectNickname).orElse(null);
+            if (member == null || tokenType == null || !tokenType.equals("refresh"))
+                throw new NotExistsRefreshTokenException();
+
+            final String dbRefreshToken = member.getRefreshToken().getToken();
+            if (!dbRefreshToken.equals(reIssueTokenReq.refreshToken()))
+                throw new NotExistsRefreshTokenException();
 
             final String reIssuedRefreshToken = jwtService.generateRefreshToken(subjectNickname);
             final RefreshToken newRefreshToken = RefreshToken.builder().token(reIssuedRefreshToken).build();
             refreshTokenRepository.updateToken(newRefreshToken.getToken());
             final String reIssuedAccessToken = jwtService.generateAccessToken(subjectNickname);
-            final String refreshCookie = ResponseCookie.from("refreshToken", reIssuedRefreshToken)
-                    .path("/")
-                    .httpOnly(true)
-                    .sameSite("None")
-                    .secure(true)
-                    .build()
-                    .toString();
+            final String reIssuedTokenResponse =
+                    objectMapper.writeValueAsString(TokenResponse.of(reIssuedAccessToken, reIssuedRefreshToken));
 
-            response.setHeader(SET_COOKIE, refreshCookie);
-            response.setHeader(AUTHORIZATION, BEARER + reIssuedAccessToken);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().println(reIssuedTokenResponse);
             return;
         }
 
@@ -112,6 +123,66 @@ public class JwtValidationFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request, response);
     }
+
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request,
+//                                    HttpServletResponse response,
+//                                    FilterChain filterChain) throws ServletException, IOException {
+//        if (requestURIContainsExcludeURI(request)) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+//        if (request.getRequestURI().equals(reIssuedTokenPath)) {
+//            final String refreshCookieValue = extractRefreshCookieValue(request);
+//            if (refreshCookieValue == null)
+//                return;
+//
+//            final String subjectNickname = jwtService.extractClaim(
+//                    refreshCookieValue, JwtService.TokenType.REFRESH, Claims::getSubject);
+//            Member member = memberRepository.findByNickname(subjectNickname).orElse(null);
+//            if (member == null)
+//                return;
+//
+//            final String reIssuedRefreshToken = jwtService.generateRefreshToken(subjectNickname);
+//            final RefreshToken newRefreshToken = RefreshToken.builder().token(reIssuedRefreshToken).build();
+//            refreshTokenRepository.updateToken(newRefreshToken.getToken());
+//            final String reIssuedAccessToken = jwtService.generateAccessToken(subjectNickname);
+//            final String refreshCookie = ResponseCookie.from("refreshToken", reIssuedRefreshToken)
+//                    .path("/")
+//                    .httpOnly(true)
+//                    .sameSite("None")
+//                    .secure(true)
+//                    .build()
+//                    .toString();
+//
+//            response.setHeader(SET_COOKIE, refreshCookie);
+//            response.setHeader(AUTHORIZATION, BEARER + reIssuedAccessToken);
+//            return;
+//        }
+//
+//        final String authHeader = request.getHeader(AUTHORIZATION);
+//        if (authHeader == null || !authHeader.startsWith(BEARER)) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+//
+//        final String accessToken = authHeader.substring(7);
+//        if (jwtService.isAccessTokenValid(accessToken)) {
+//            final String userEmail = jwtService.extractAllClaims(
+//                    accessToken, JwtService.TokenType.ACCESS).get("email", String.class);
+//            final Member member = memberRepository.findByEmail(userEmail).orElse(null);
+//            if (member != null) {
+//                final AuthenticatedMember authenticatedMember = AuthenticatedMember.from(
+//                        MemberDto.from(member)
+//                );
+//                authenticatedMember.setOriginalEntity(member);
+//                final UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken
+//                        .authenticated(authenticatedMember, null, authenticatedMember.getAuthorities());
+//                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//            }
+//        }
+//        filterChain.doFilter(request, response);
+//    }
 
     private String extractRefreshCookieValue(HttpServletRequest httpServletRequest) {
         return Stream.of(httpServletRequest.getCookies())
