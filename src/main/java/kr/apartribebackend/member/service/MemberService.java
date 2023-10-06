90de050099f68741236a2d74d65406f751ba6453
Revi1337 <david122123@gmail.com>
2023-10-06 20:39:09 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/kr/apartribebackend/member/service/MemberService.java
+++ src/main/java/kr/apartribebackend/member/service/MemberService.java
@@ -1,20 +1,14 @@
 package kr.apartribebackend.member.service;
 
 import kr.apartribebackend.member.domain.Member;
-import kr.apartribebackend.member.dto.MemberChangePasswordReq;
 import kr.apartribebackend.member.dto.MemberDto;
 import kr.apartribebackend.member.exception.*;
-import kr.apartribebackend.member.principal.AuthenticatedMember;
 import kr.apartribebackend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 @Transactional
 @RequiredArgsConstructor
@@ -22,8 +16,6 @@ import org.springframework.util.StringUtils;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
-    private final PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
     public MemberDto findSingleMember(final String email) {
@@ -46,13 +38,6 @@ public class MemberService {
                         () -> { throw new UserCantUpdateException(); });
     }
 
-    public void deleteSingleUser(final String email) {
-        memberRepository.findByEmail(email)
-                .ifPresentOrElse(
-                        memberRepository::delete, () -> { throw new UserCantDeleteException(); }
-                );
-    }
-
     public Page<MemberDto> findAllMembers(final Pageable pageable) {
         return memberRepository.findAll(pageable)
                 .map(MemberDto::from);
@@ -61,21 +46,6 @@ public class MemberService {
     @Transactional(readOnly = true)
     public boolean existsByNickname(final String nickname) {
         return memberRepository.existsByNickname(nickname);
-    }
-
-    public void updateSingleMemberPassword(final AuthenticatedMember authenticatedMember,
-                                           final MemberChangePasswordReq memberChangePasswordReq) {
-        if (!passwordEncoder.matches(memberChangePasswordReq.currentPassword(), authenticatedMember.getPassword()))
-            throw new UserCantUpdatePasswordException();
-        final Member member = authenticatedMember.getOriginalEntity();
-        member.changePassword(memberChangePasswordReq.newPassword());
-    }
-
-    public void updateSingleMemberNickname(final AuthenticatedMember authenticatedMember, final String nickname) {
-        if (existsByNickname(nickname))
-            throw new UserCantUpdateNicknameException();
-        final Member member = authenticatedMember.getOriginalEntity();
-        member.updateNickname(nickname);
     }
 
 }
