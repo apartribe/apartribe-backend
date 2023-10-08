ac5eaac6a42ef9ee7e0f26e593d74cc6cfb3a2e7
Revi1337 <david122123@gmail.com>
2023-10-08 13:27:36 +0900

    feat: 카테고리를 상속받는 서브 카테고리 도메인 작성

---
+++ src/main/java/kr/apartribebackend/category/domain/TogetherCategory.java
@@ -0,0 +1,17 @@
+package kr.apartribebackend.category.domain;
+
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter @SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue(value = "TOGETHER")
+public class TogetherCategory extends Category {
+
+}
