9614e33e814d0713f554f579a7a22dd0357fcfd3
Revi1337 <david122123@gmail.com>
2023-10-12 12:56:10 +0900

    feat: apart 도메인 작성

---
+++ src/main/java/kr/apartribebackend/apart/domain/Apartment.java
@@ -0,0 +1,67 @@
+package kr.apartribebackend.apart.domain;
+
+
+import jakarta.persistence.*;
+import kr.apartribebackend.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "APARTMENT",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "code", columnNames = "CODE"),
+                @UniqueConstraint(name = "name", columnNames = "NAME"),
+        }
+)
+public class Apartment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "APART_ID")
+    private Long id;
+
+    @Column(name = "CODE", nullable = false)
+    private String code;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "apartment")
+    private final Set<Member> members = new HashSet<>();
+
+    @Builder
+    private Apartment(Long id, String code, String name) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Apartment apartment)) return false;
+        return id != null && Objects.equals(id, apartment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    /////////////////////////////// BUSINESS LOGIC ///////////////////////////////
+
+//    public void addMember(Member member) {
+//        if (member.getApartment() != this) {
+//            member.changeApartment(this);
+//        }
+//        this.members.add(member);
+//    }
+
+}
