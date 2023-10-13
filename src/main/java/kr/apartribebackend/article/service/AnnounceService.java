2f02385fd767128f1bd8f0e865e45b6a0c5da04a
Revi1337 <david122123@gmail.com>
2023-10-13 17:18:28 +0900

    feat: announce 게시글 수정 api 작성

--- src/main/java/kr/apartribebackend/article/service/AnnounceService.java
+++ src/main/java/kr/apartribebackend/article/service/AnnounceService.java
@@ -9,6 +9,7 @@ import kr.apartribebackend.article.exception.ArticleNotFoundException;
 import kr.apartribebackend.article.exception.CannotReflectLikeToArticleException;
 import kr.apartribebackend.article.repository.announce.AnnounceRepository;
 import kr.apartribebackend.attachment.domain.Attachment;
+import kr.apartribebackend.attachment.repository.AttachmentRepository;
 import kr.apartribebackend.attachment.service.AttachmentService;
 import kr.apartribebackend.member.domain.Member;
 import kr.apartribebackend.member.dto.MemberDto;
@@ -30,6 +31,7 @@ public class AnnounceService {
     private final AnnounceRepository announceRepository;
 
     private final AttachmentService attachmentService;
+    private final AttachmentRepository attachmentRepository;
 
     @Transactional(readOnly = true)
     public Page<AnnounceDto> findMultipleAnnouncesByLevel(final Level level,
@@ -51,6 +53,18 @@ public class AnnounceService {
                 .ifPresentOrElse(Board::reflectArticleLike,
                         () -> { throw new CannotReflectLikeToArticleException(); });
     }
+
+//    public void removeArticle(final Board board) {
+//        final List<Comment> comments = commentRepository.findCommentsForBoard(board);
+//        final List<Comment> children = comments.stream()
+//                .filter(comment -> !comment.getChildren().isEmpty())
+//                .flatMap(comment -> comment.getChildren().stream())
+//                .toList();
+//
+//        commentRepository.deleteAllInBatch(children);
+//        commentRepository.deleteAllInBatch(comments);
+//        boardRepository.delete(board);
+//    }
 
     public SingleAnnounceResponse findSingleAnnounceById(final Long announceId) {
         return announceRepository.findJoinedAnnounceById(announceId)
@@ -75,6 +89,17 @@ public class AnnounceService {
             attachment.registBoard(announce);
         }
         attachmentService.saveAttachments(attachments);
+    }
+
+    public SingleAnnounceResponse updateAnnounce(final Long announceId,
+                                                 final AnnounceDto announceDto,
+                                                 final MemberDto memberDto) {
+        final Announce announceEntity = announceRepository.findById(announceId)
+                .orElseThrow(ArticleNotFoundException::new);
+        // TODO 토큰에서 뽑아온 사용자 정보와 작성된 게시물의 createdBy 를 검증해야하지만, 지금은 Dummy 라 검증할 수가 없다. 알아두자.
+        final Announce updatedAnnounce = announceEntity
+                .updateAnnounce(announceDto.getLevel(), announceDto.getTitle(), announceDto.getContent());
+        return SingleAnnounceResponse.from(updatedAnnounce);
     }
 
 }
