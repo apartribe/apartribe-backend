ee3954ad2b75b6c248bda1904afcd1651955f873
Revi1337 <david122123@gmail.com>
2023-10-19 01:41:27 +0900

    feat: spring batch job 를 스케줄링에 등록

--- src/main/java/kr/apartribebackend/global/config/SchedulingConfig.java
+++ src/main/java/kr/apartribebackend/global/config/SchedulingConfig.java
@@ -4,12 +4,21 @@ import kr.apartribebackend.token.email.config.EmailTokenContextHolder;
 import kr.apartribebackend.token.email.domain.EmailToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Map;
 
 @Slf4j
@@ -22,6 +31,13 @@ public class SchedulingConfig {
             EmailTokenContextHolder emailTokenContextHolder
     ) {
         return new ClearEmailTokenContextHolderTask(emailTokenContextHolder);
+    }
+
+    @Bean
+    public UpdateRecruitStatusTask updateRecruitStatusTask(
+            JobLauncher jobLauncher, Job updateRecruitStatusJob
+    ) {
+        return new UpdateRecruitStatusTask(jobLauncher, updateRecruitStatusJob);
     }
 
     @RequiredArgsConstructor
@@ -43,6 +59,25 @@ public class SchedulingConfig {
                 }
             }
         }
+    }
+
+    @RequiredArgsConstructor
+    static class UpdateRecruitStatusTask {
+
+        private final JobLauncher jobLauncher;
+        private final Job updateRecruitStatusJob;
+
+        @Scheduled(cron = "0 0 0 * * *")
+        public void helloWorldJobRun() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+            JobParameters jobParameters = new JobParameters(
+                    Collections.singletonMap(
+                            "requestTime",
+                            new JobParameter<>(System.currentTimeMillis(), Long.class)
+                    )
+            );
+            jobLauncher.run(updateRecruitStatusJob, jobParameters);
+        }
+
     }
 
 }
