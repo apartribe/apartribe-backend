5c86e56ffb6759ff43c0c19ba21419568ede9813
Revi1337 <david122123@gmail.com>
2023-10-18 20:43:14 +0900

    fix: 인증객체와 dto 에 아파트 정보 및 회원생성일 추가

--- src/main/java/kr/apartribebackend/member/principal/AuthenticatedMember.java
+++ src/main/java/kr/apartribebackend/member/principal/AuthenticatedMember.java
@@ -1,14 +1,17 @@
 package kr.apartribebackend.member.principal;
 
+import kr.apartribebackend.apart.dto.ApartmentDto;
 import kr.apartribebackend.member.domain.Member;
 import kr.apartribebackend.member.dto.MemberDto;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
+import java.time.LocalDateTime;
 import java.util.Set;
 
+@Getter
 public class AuthenticatedMember implements UserDetails {
 
     private Long id;
@@ -16,6 +19,9 @@ public class AuthenticatedMember implements UserDetails {
     private String password;
     private String name;
     private String nickname;
+    private String apartCode;
+    private String apartName;
+    private LocalDateTime createdAt;
     private Set<? extends GrantedAuthority> authorities;
     private Member originalEntity;
 
@@ -28,6 +34,9 @@ public class AuthenticatedMember implements UserDetails {
                                String password,
                                String name,
                                String nickname,
+                               String apartCode,
+                               String apartName,
+                               LocalDateTime createdAt,
                                Set<? extends GrantedAuthority> authorities) {
         this.id = id;
         this.email = email;
@@ -35,6 +44,9 @@ public class AuthenticatedMember implements UserDetails {
         this.name = name;
         this.nickname = nickname;
         this.authorities = authorities;
+        this.apartCode = apartCode;
+        this.createdAt = createdAt;
+        this.apartName = apartName;
     }
 
     public static AuthenticatedMember from(MemberDto memberDto) {
@@ -44,52 +56,41 @@ public class AuthenticatedMember implements UserDetails {
                 memberDto.getPassword(),
                 memberDto.getName(),
                 memberDto.getNickname(),
+                "EMPTY",
+                "EMPTY",
+                memberDto.getCreatedAt(),
+                Set.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+
+    public static AuthenticatedMember from(MemberDto memberDto, ApartmentDto apartmentDto) {
+        return new AuthenticatedMember(
+                memberDto.getId(),
+                memberDto.getEmail(),
+                memberDto.getPassword(),
+                memberDto.getName(),
+                memberDto.getNickname(),
+                apartmentDto.getCode(),
+                apartmentDto.getName(),
+                memberDto.getCreatedAt(),
                 Set.of(new SimpleGrantedAuthority("ROLE_USER"))
         );
     }
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
+    public String getUsername() { return nickname; }
 
     @Override
-    public String getPassword() {
-        return password;
-    }
+    public boolean isAccountNonExpired() { return true; }
 
     @Override
-    public String getUsername() {
-        return nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Member getOriginalEntity() {
-        return originalEntity;
-    }
+    public boolean isAccountNonLocked() { return true; }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+    public boolean isCredentialsNonExpired() { return true; }
 
     @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+    public boolean isEnabled() { return true; }
 
     public MemberDto toDto() {
         return MemberDto.builder()
@@ -102,74 +103,3 @@ public class AuthenticatedMember implements UserDetails {
     }
 
 }
-
-
-//public record AuthenticatedMember(
-//        Long id,
-//        String email,
-//        String password,
-//        String name,
-//        String nickname,
-//        Set<? extends GrantedAuthority> authorities
-//) implements UserDetails {
-//
-//    public static AuthenticatedMember from(MemberDto memberDto) {
-//        return new AuthenticatedMember(
-//                memberDto.getId(),
-//                memberDto.getEmail(),
-//                memberDto.getPassword(),
-//                memberDto.getName(),
-//                memberDto.getNickname(),
-//                Set.of(new SimpleGrantedAuthority("ROLE_USER"))
-//        );
-//    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return authorities;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return nickname;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//
-//    public MemberDto toDto() {
-//        return MemberDto.builder()
-//                .id(id)
-//                .email(email)
-//                .name(name)
-//                .nickname(nickname)
-//                .password(password)
-//                .build();
-//    }
-//}
