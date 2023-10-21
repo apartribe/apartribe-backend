23e1cbba6227748a4372b4fc2e65f37840c7185c
Revi1337 <david122123@gmail.com>
2023-10-21 18:53:57 +0900

    feat: 공지사항 작성 request 에 floatFrom, floatTo 필드 추가

--- src/main/java/kr/apartribebackend/article/dto/announce/AppendAnnounceReq.java
+++ src/main/java/kr/apartribebackend/article/dto/announce/AppendAnnounceReq.java
@@ -1,9 +1,11 @@
 package kr.apartribebackend.article.dto.announce;
 
 import jakarta.validation.constraints.NotEmpty;
+import kr.apartribebackend.article.annotation.LocalDateIsValid;
 import kr.apartribebackend.article.domain.Level;
 import kr.apartribebackend.article.annotation.IsLevelValid;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 // TODO thumbnail 에 url 이 담기기 떄문에 악의적인 문자를 필터링해야한다.
@@ -11,6 +13,8 @@ public record AppendAnnounceReq(
         @NotEmpty(message = "공지사항 레벨은 공백일 수 없습니다") @IsLevelValid String level,
         @NotEmpty(message = "제목은 공백일 수 없습니다") String title,
         @NotEmpty(message = "내용은 공백일 수 없습니다") String content,
+        @NotEmpty(message = "위젯에 떠있을 시간은 공백일 수 없습니다.") @LocalDateIsValid String floatFrom,
+        @NotEmpty(message = "위젯에 떠있을 시간은 공백일 수 없습니다.") @LocalDateIsValid String floatTo,
         String thumbnail
 ) {
 
@@ -23,6 +27,8 @@ public record AppendAnnounceReq(
                 .title(title)
                 .content(content)
                 .thumbnail(thumbnail == null ? "" : thumbnail)
+                .floatFrom(LocalDate.parse(floatFrom))
+                .floatTo(LocalDate.parse(floatTo))
                 .build();
     }
 }
