e2e944751d1b2a035f8a4601fdd9bce7f6d96eb8
Revi1337 <david122123@gmail.com>
2023-10-23 16:59:37 +0900

    fix: 커뮤니티 게시글 단일 응답 response 에서 댓글 수 삭제

--- src/main/java/kr/apartribebackend/article/dto/SingleArticleResponse.java
+++ src/main/java/kr/apartribebackend/article/dto/SingleArticleResponse.java
@@ -1,6 +1,7 @@
 package kr.apartribebackend.article.dto;
 
 import kr.apartribebackend.article.domain.Article;
+import kr.apartribebackend.member.domain.Member;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,33 +10,86 @@ import java.util.stream.Collectors;
 public record SingleArticleResponse(
         Long id,
         String createdBy,
+        String profileImage,
         LocalDateTime createdAt,
         String category,
         String title,
         String content,
         int liked,
-        int saw,
-        long commentCounts
-//        List<SingleCommentResponse> comments
+        int saw
 ) {
     public static SingleArticleResponse from(Article article) {
         article.reflectArticleSaw();
-        List<SingleCommentResponse> commentResponses = article.getComments()
-                .stream()
-                .map(SingleCommentResponse::from)
-                .toList();
         return new SingleArticleResponse(
                 article.getId(),
                 article.getCreatedBy(),
+                "",
                 article.getCreatedAt(),
                 article.getCategory().getName(),
                 article.getTitle(),
                 article.getContent(),
                 article.getLiked(),
-                article.getSaw(),
-                commentResponses.size()
-//                commentResponses
+                article.getSaw()
+        );
+    }
+
+    public static SingleArticleResponse from(Article article, Member member) {
+        article.reflectArticleSaw();
+        return new SingleArticleResponse(
+                article.getId(),
+                article.getCreatedBy(),
+                member.getProfileImageUrl(),
+                article.getCreatedAt(),
+                article.getCategory().getName(),
+                article.getTitle(),
+                article.getContent(),
+                article.getLiked(),
+                article.getSaw()
         );
     }
 
 }
+
+
+
+//package kr.apartribebackend.article.dto;
+//
+//import kr.apartribebackend.article.domain.Article;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//public record SingleArticleResponse(
+//        Long id,
+//        String createdBy,
+//        LocalDateTime createdAt,
+//        String category,
+//        String title,
+//        String content,
+//        int liked,
+//        int saw,
+//        long commentCounts
+////        List<SingleCommentResponse> comments
+//) {
+//    public static SingleArticleResponse from(Article article) {
+//        article.reflectArticleSaw();
+//        List<SingleCommentResponse> commentResponses = article.getComments()
+//                .stream()
+//                .map(SingleCommentResponse::from)
+//                .toList();
+//        return new SingleArticleResponse(
+//                article.getId(),
+//                article.getCreatedBy(),
+//                article.getCreatedAt(),
+//                article.getCategory().getName(),
+//                article.getTitle(),
+//                article.getContent(),
+//                article.getLiked(),
+//                article.getSaw(),
+//                commentResponses.size()
+////                commentResponses
+//        );
+//    }
+//
+//}
