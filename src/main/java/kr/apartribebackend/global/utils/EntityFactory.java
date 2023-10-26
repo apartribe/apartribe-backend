6da7ac154932b38f497680e7f31fc4969a719dcf
Revi1337 <david122123@gmail.com>
2023-10-26 23:51:04 +0900

    feat: global 엔티티 팩토리 작성

---
+++ src/main/java/kr/apartribebackend/global/utils/EntityFactory.java
@@ -0,0 +1,22 @@
+package kr.apartribebackend.global.utils;
+
+import kr.apartribebackend.apart.domain.Apartment;
+import kr.apartribebackend.category.domain.ArticleCategory;
+import kr.apartribebackend.category.domain.TogetherCategory;
+
+
+public class EntityFactory {
+
+    public static ArticleCategory.ArticleCategoryBuilder<?, ?> createArticleCategory(Apartment apartment, String categoryName) {
+        return ArticleCategory.builder()
+                .apartment(apartment)
+                .name(categoryName);
+    }
+
+    public static TogetherCategory.TogetherCategoryBuilder<?, ?> createTogetherCategory(Apartment apartment, String categoryName) {
+        return TogetherCategory.builder()
+                .apartment(apartment)
+                .name("categoryName");
+    }
+
+}
