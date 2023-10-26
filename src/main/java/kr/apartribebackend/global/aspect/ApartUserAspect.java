f3ab6fc0799ac3819aa25b2a68769f5f80d3b53c
Revi1337 <david122123@gmail.com>
2023-10-26 16:24:49 +0900

    fix: 로그인사용자와 아파트인증사용자를 구분하는 aop 코드 수정

--- src/main/java/kr/apartribebackend/global/aspect/ApartUserAspect.java
+++ src/main/java/kr/apartribebackend/global/aspect/ApartUserAspect.java
@@ -1,7 +1,9 @@
 package kr.apartribebackend.global.aspect;
 
 import kr.apartribebackend.apart.exception.NeedApartmentAuthException;
+import kr.apartribebackend.apart.exception.NeedToMakeCommunityException;
 import kr.apartribebackend.global.annotation.ApartUser;
+import kr.apartribebackend.member.domain.AuthStatus;
 import kr.apartribebackend.member.principal.AuthenticatedMember;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -18,9 +20,10 @@ public class ApartUserAspect {
     public void apartUserPointcut(final ApartUser apartUser) {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         final AuthenticatedMember authenticatedMember = (AuthenticatedMember) authentication.getPrincipal();
-        if (!authenticatedMember.isAuthenticated()) {
+        if (authenticatedMember.getAuthStatus() == AuthStatus.INCOMPLETE) {
             throw new NeedApartmentAuthException();
+        } else if (authenticatedMember.getAuthStatus() == AuthStatus.PENDING) {
+            throw new NeedToMakeCommunityException();
         }
     }
-
 }
