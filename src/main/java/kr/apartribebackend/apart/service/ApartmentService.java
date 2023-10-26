830399206fcc4be7d26cd97b00825e64ae5cd432
Revi1337 <david122123@gmail.com>
2023-10-26 23:54:25 +0900

    feat: 아파트 서버 및 커뮤니티 생성 api 작성

--- src/main/java/kr/apartribebackend/apart/service/ApartmentService.java
+++ src/main/java/kr/apartribebackend/apart/service/ApartmentService.java
@@ -5,12 +5,17 @@ import kr.apartribebackend.apart.domain.Apartment;
 import kr.apartribebackend.apart.dto.ApartmentDto;
 import kr.apartribebackend.apart.exception.*;
 import kr.apartribebackend.apart.repository.ApartmentRepository;
+import kr.apartribebackend.category.domain.ArticleCategory;
+import kr.apartribebackend.category.domain.TogetherCategory;
+import kr.apartribebackend.category.repository.CategoryRepository;
+import kr.apartribebackend.global.utils.EntityFactory;
 import kr.apartribebackend.member.domain.Member;
 import kr.apartribebackend.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 import static org.springframework.util.StringUtils.*;
 
@@ -21,172 +26,97 @@ import static org.springframework.util.StringUtils.*;
 public class ApartmentService {
 
     private final ApartmentRepository apartmentRepository;
+    private final CategoryRepository categoryRepository;
 
+    /**
+     * 1. Member 가 Apart 에 등록되어있으면 Exception
+     * 2. 인증한 Apart 가 존재하면 Member 에 Apart 를 등록하고, AUTHSTATUS 를 COMPLETED 로 설정
+     * 3. 인증한 Apart 가 존재하지 않으면 AUTHSTATUS 를 PENDING 으로 설정
+     * @param memberDto
+     * @param apartmentDto
+     * @param member
+     */
     public void authenticateApartment(final MemberDto memberDto,
                                       final ApartmentDto apartmentDto,
                                       final Member member) {
-        checkMemberHaveApartments(member);                                          // Member 가 Apart 에 등록되어있으면 Exception
-        if (hasText(member.getApartCode()) || hasText(member.getApartName())) {     // Member 가 Apart 인증이 되어있으면 Exception
+        checkMemberHaveApartments(member);
+        if (hasText(member.getApartCode()) || hasText(member.getApartName())) {
             throw new AlreadyAuthenticateApartException();
         }
         final Apartment findApartment = apartmentRepository.findApartmentByCode(apartmentDto.getCode())
                 .orElse(null);
-        if (findApartment != null) {                                                // 인증한 Apart 가 존재하면 Member 에 Apart 를 등록하고, AUTHSTATUS 를 COMPLETED 로 설정
+        if (findApartment != null) {
             log.info("인증하신 아파트는 사이트에 이미 존재하니, 사용자를 아파트에 등록하고, COMPLETED 로 처리합니다.");
             member.changeApartment(findApartment);
             member.authenticateApartInfo(apartmentDto.getCode(), apartmentDto.getName());
-        } else {                                                                    // 인증한 Apart 가 존재하지 않으면 AUTHSTATUS 를 PENDING 으로 설정
+        } else {
             log.info("인증하신 아파트는 사이트에 존재하지 않으니, PENDING 으로 처리합니다.");
             member.pendingApartInfo(apartmentDto.getCode(), apartmentDto.getName());
         }
     }
 
+    /**
+     * 1. Member 가 Apart 에 등록되어있으면 Exception
+     * 2. 생성하려는 아파트 커뮤니티가 존재하면 Exception
+     * 3. 만들으려는 아파트 커뮤니티와 사전에 인증한 아파트 정보와 일치하지 않으면 Exception
+     *
+     * - Apart 엔티티를 저장
+     * - Member 에 Apart 를 등록하고
+     * - 디폴트 커뮤니티를 등록하고, AUTHSTATUS 를 COMPLETED 로 설정
+     * @param memberDto
+     * @param apartmentDto
+     * @param member
+     */
     public void constructCommunity(final MemberDto memberDto,
                                    final ApartmentDto apartmentDto,
                                    final Member member) {
-        checkMemberHaveApartments(member);                                          // Member 가 Apart 에 등록되어있으면 Exception
+        checkMemberHaveApartments(member);
         if (apartmentRepository.existsByCodeAndName(apartmentDto.getCode(), apartmentDto.getName())) {
-            throw new ApartAlreadyExistsException();                                // 만들으려는 아파트 커뮤니티가 존재하면 Exception
+            throw new ApartAlreadyExistsException();
         }
-        if (                                                                        // 만들으려는 아파트 커뮤니티와 사전에 인증한 아파트 정보와 일치하지 않으면 Exception
+        if (
                 !member.getApartCode().equals(apartmentDto.getCode()) ||
                 !member.getApartName().equals(apartmentDto.getName())
         ) {
             throw new ApartmentMemberIntegrityException();
         }
         final Apartment apartment = apartmentRepository.save(apartmentDto.toEntity());
+        log.info("새로운 아파트 커뮤니티가 생성됩니다.");
         member.changeApartment(apartment);
         member.authenticateApartInfo(apartmentDto.getCode(), apartmentDto.getName());
+        buildDefaultCommunity(member, apartment);
     }
 
-    public boolean existsByCode(String apartCode) {
+    public boolean existsByCode(final String apartCode) {
         return apartmentRepository.existsByCode(apartCode);
     }
 
-    public ApartmentDto findApartByCode(String apartCode) {
+    public ApartmentDto findApartByCode(final String apartCode) {
         return apartmentRepository.findApartmentByCode(apartCode)
                 .map(ApartmentDto::from)
                 .orElseThrow(ApartNonExistsException::new);
     }
 
-    private void checkMemberHaveApartments(Member member) {
+    private void checkMemberHaveApartments(final Member member) {
         if (member.getApartment() != null) {
             throw new ApartMemberDuplicateException();
         }
     }
 
+    private void buildDefaultCommunity(final Member member, final Apartment apartment) {
+        ArticleCategory articleCategory1 = EntityFactory.createArticleCategory(apartment, "자유 게시판").build();
+        ArticleCategory articleCategory2 = EntityFactory.createArticleCategory(apartment, "고양이 집사 모임").build();
+        ArticleCategory articleCategory3 = EntityFactory.createArticleCategory(apartment, "신혼 부부 정보 공유").build();
+        TogetherCategory togetherCategory1 = EntityFactory.createTogetherCategory(apartment, "동호회").build();
+        TogetherCategory togetherCategory2 = EntityFactory.createTogetherCategory(apartment, "친목회").build();
+        TogetherCategory togetherCategory3 = EntityFactory.createTogetherCategory(apartment, "카풀").build();
+        TogetherCategory togetherCategory4 = EntityFactory.createTogetherCategory(apartment, "공동구매").build();
+        categoryRepository.saveAll(
+                List.of(
+                        articleCategory1, articleCategory2, articleCategory3,
+                        togetherCategory1, togetherCategory2, togetherCategory3, togetherCategory4
+                )
+        );
+    }
+
 }
-
-
-
-//package kr.apartribebackend.apart.service;
-//
-//
-//import kr.apartribebackend.apart.domain.Apartment;
-//import kr.apartribebackend.apart.dto.ApartmentDto;
-//import kr.apartribebackend.apart.exception.*;
-//import kr.apartribebackend.apart.repository.ApartmentRepository;
-//import kr.apartribebackend.member.domain.Member;
-//import kr.apartribebackend.member.dto.MemberDto;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.util.StringUtils;
-//
-//import static org.springframework.util.StringUtils.*;
-//
-//@Slf4j
-//@Transactional
-//@RequiredArgsConstructor
-//@Service
-//public class ApartmentService {
-//
-//    private final ApartmentRepository apartmentRepository;
-//
-//    public void authenticateApartment(final MemberDto memberDto,
-//                                      final ApartmentDto apartmentDto,
-//                                      final Member member) {
-//        checkMemberHaveApartments(memberDto);
-//        if (hasText(member.getApartCode()) || hasText(member.getApartName())) {
-//            throw new AlreadyAuthenticateApartException();
-//        }
-//        final Apartment findApartment = apartmentRepository.findApartmentByCode(apartmentDto.getCode())
-//                .orElse(null);
-//        if (findApartment != null) {
-//            member.changeApartment(findApartment);
-//            member.updateApartInfo(apartmentDto.getCode(), apartmentDto.getName());
-//        } else {
-//            member.rememberApartInfo(apartmentDto.getCode(), apartmentDto.getName());
-//        }
-//    }
-//
-//    public void constructCommunity(final MemberDto memberDto,
-//                                   final ApartmentDto apartmentDto,
-//                                   final Member member) {
-//        checkMemberHaveApartments(memberDto);
-//        if (apartmentRepository.existsByCodeAndName(apartmentDto.getCode(), apartmentDto.getName())) {
-//            throw new ApartAlreadyExistsException();
-//        }
-//        if (
-//                !member.getApartCode().equals(apartmentDto.getCode()) ||
-//                !member.getApartName().equals(apartmentDto.getName())
-//        ) {
-//            throw new ApartmentMemberIntegrityException();
-//        }
-//        final Apartment apartment = apartmentRepository.save(apartmentDto.toEntity());
-//        member.changeApartment(apartment);
-//        member.updateApartInfo(apartmentDto.getCode(), apartmentDto.getName());
-//    }
-//
-//    public boolean existsByCode(String apartCode) {
-//        return apartmentRepository.existsByCode(apartCode);
-//    }
-//
-//    public ApartmentDto findApartByCode(String apartCode) {
-//        return apartmentRepository.findApartmentByCode(apartCode)
-//                .map(ApartmentDto::from)
-//                .orElseThrow(ApartNonExistsException::new);
-//    }
-//
-//    private void checkMemberHaveApartments(MemberDto memberDto) {
-//        if (
-//                !memberDto.getApartmentDto().getCode().equals("EMPTY") ||
-//                !memberDto.getApartmentDto().getName().equals("EMPTY")
-//        ) {
-//            throw new ApartMemberDuplicateException();
-//        }
-//    }
-//}
-
-
-//    public void authenticateApartment(final MemberDto memberDto,
-//                                      final ApartmentDto apartmentDto,
-//                                      final Member member) {
-//        checkMemberHaveApartments(memberDto);
-//        if (
-//                StringUtils.hasText(member.getApartCode()) ||
-//                StringUtils.hasText(member.getApartName())
-//        ) {
-//            throw new AlreadyAuthenticateApartException();
-//        }
-//        member.rememberApartInfo(apartmentDto.getCode(), apartmentDto.getName());
-//    }
-
-//    public void constructCommunity(final MemberDto memberDto,
-//                                   final ApartmentDto apartmentDto,
-//                                   final Member member) {
-//        checkMemberHaveApartments(memberDto);
-//        if (apartmentRepository.existsByCodeAndName(apartmentDto.getCode(), apartmentDto.getName())) {
-//            throw new ApartAlreadyExistsException();
-//        }
-//        if (
-//                !member.getApartCode().equals(apartmentDto.getCode()) ||
-//                        !member.getApartName().equals(apartmentDto.getName())
-//        ) {
-//            throw new ApartmentMemberIntegrityException();
-//        }
-//        final Apartment apartment = apartmentRepository.save(apartmentDto.toEntity());
-//        member.changeApartment(apartment);
-//        member.updateApartInfo(apartmentDto.getCode(), apartmentDto.getName());
-//    }
