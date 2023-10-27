2b4b12a359d1714ba3f6ef8458808d959514b2c3
Revi1337 <david122123@gmail.com>
2023-10-27 17:37:46 +0900

    feat: 회원가입 api 에 제휴 및 동의란 추가

--- src/main/java/kr/apartribebackend/member/dto/MemberJoinReq.java
+++ src/main/java/kr/apartribebackend/member/dto/MemberJoinReq.java
@@ -1,8 +1,7 @@
 package kr.apartribebackend.member.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
+import kr.apartribebackend.member.dto.agreements.AgreementsDto;
 import org.springframework.util.StringUtils;
 
 // TODO profileImageUrl 악의적인 문자를 필터링해야 한다.
@@ -13,10 +12,15 @@ public record MemberJoinReq(
         @NotEmpty(message = "비밀번호확인은 공백일 수 없습니다") String passwordConfirm,
         @NotEmpty(message = "이름은 공백일 수 없습니다") String name,
         @NotEmpty(message = "닉네임은 공백일 수 없습니다") String nickname,
-        String profileImageUrl
+        String profileImageUrl,
+        @NotNull(message = "만 14 세 이상입니다 확인란은 체크하셔야 합니다.") @AssertTrue Boolean goeFourteen,
+        @NotNull(message = "저작권 안내 확인란은 체크하셔야 합니다.") @AssertTrue Boolean confirmCopyright,
+        @NotNull(message = "이용약관동의 확인란은 체크하셔야 합니다.") @AssertTrue Boolean useAgree,
+        @NotNull(message = "개인정보 수집 및 이용 동의 확인란은 체크하셔야 합니다.") @AssertTrue Boolean dataCollectAgree,
+        @NotNull(message = "홍보 및 마케팅 이용 동의 확인란은 체크하셔야 합니다.") Boolean advertiseAgree
 ) {
 
-    public MemberDto toDto() {
+    public MemberDto toMemberDto() {
         return MemberDto.builder()
                 .email(email)
                 .password(password)
@@ -26,4 +30,13 @@ public record MemberJoinReq(
                 .build();
     }
 
+    public AgreementsDto toAgreementsDto() {
+        return AgreementsDto.builder()
+                .goeFourteen(goeFourteen)
+                .confirmCopyright(confirmCopyright)
+                .useAgree(useAgree)
+                .dataCollectAgree(dataCollectAgree)
+                .advertiseAgree(advertiseAgree)
+                .build();
+    }
 }
