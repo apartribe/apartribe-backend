a45c3d7df767a71f07be6b33b73628e005095f27
Revi1337 <david122123@gmail.com>
2023-10-29 12:12:57 +0900

    fix: announce widget list response 에 content 를 title 로변경

--- src/main/java/kr/apartribebackend/article/dto/announce/AnnounceWidgetRes.java
+++ src/main/java/kr/apartribebackend/article/dto/announce/AnnounceWidgetRes.java
@@ -5,11 +5,11 @@ import kr.apartribebackend.article.domain.Announce;
 public record AnnounceWidgetRes(
         Long id,
         String level,
-        String content
+        String title
 ) {
     public static AnnounceWidgetRes from(Announce announce) {
         return new AnnounceWidgetRes(
-                announce.getId(), announce.getLevel().getName(), announce.getContent()
+                announce.getId(), announce.getLevel().getName(), announce.getTitle()
         );
     }
 
