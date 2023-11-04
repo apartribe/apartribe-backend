06e324bd4bfada03b4df68be8c2e3a990ca7b49c
Revi1337 <david122123@gmail.com>
2023-11-04 11:04:23 +0900

    coment: annotation aop 역할 주석 작성

--- src/main/java/kr/apartribebackend/global/annotation/ApartUser.java
+++ src/main/java/kr/apartribebackend/global/annotation/ApartUser.java
@@ -8,5 +8,12 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ApartUser {
+
+    /**
+     * 해당 옵션을 true 로 설정하면 아파트인증이 완료된 사용자임을 판단함과 동시에 PathVariable 정보와 인증객체의 apartCode 를 비교를 비교하여 HandlerMethod 에 접근 여부를 판단한다.
+     * 해당 옵션을 false 로 설정하면 아파트인증이 완료된 사용자인지만 판단한다.
+     * @return
+     */
     boolean checkApartment() default true;
+
 }
