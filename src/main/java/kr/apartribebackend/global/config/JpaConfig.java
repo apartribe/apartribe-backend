d4ca002212d39f29837d655432be8257bed40f7c
Revi1337 <david122123@gmail.com>
2023-11-12 23:26:33 +0900

    fix: jpaconfig 수정

--- src/main/java/kr/apartribebackend/global/config/JpaConfig.java
+++ src/main/java/kr/apartribebackend/global/config/JpaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -19,7 +20,7 @@ public class JpaConfig {
     public AuditorAware<String> auditorAware() {
         return () -> {
             final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (authentication == null || !authentication.isAuthenticated()) {
+            if (authentication == null || authentication.getClass().equals(AnonymousAuthenticationToken.class)) {
                 return Optional.of("AnonymousUser");
             }
             if (authentication.getPrincipal() instanceof AuthenticatedMember authenticatedMember) {
