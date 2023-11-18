186e5a51c9a49cc5bb664954bad6de94372e40a1
Revi1337 <david122123@gmail.com>
2023-11-18 21:09:49 +0900

    feat: 함께해요 게시글 응답에 onlyApartUser 필드 추가

--- src/main/java/kr/apartribebackend/article/dto/together/TogetherResponse.java
+++ src/main/java/kr/apartribebackend/article/dto/together/TogetherResponse.java
@@ -1,28 +1,73 @@
 package kr.apartribebackend.article.dto.together;
 
-import kr.apartribebackend.article.domain.Together;
+import com.querydsl.core.annotations.QueryProjection;
+import kr.apartribebackend.article.domain.RecruitStatus;
+import kr.apartribebackend.category.domain.Category;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-public record TogetherResponse(
-        Long id,
-        String category,
-        String recruitStatus,
-        String createdBy,
-        String title,
-        String thumbnail,
-        String description,
-        LocalDateTime createdAt){
-    public static TogetherResponse from(Together together) {
-        return new TogetherResponse(
-                together.getId(),
-                together.getCategory().getName(),
-                together.getRecruitStatus().getName(),
-                together.getCreatedBy(),
-                together.getTitle(),
-                together.getThumbnail(),
-                together.getDescription(),
-                together.getCreatedAt()
-        );
+@Getter
+public class TogetherResponse {
+
+    private Long id;
+    private String category;
+    private String recruitStatus;
+    private String createdBy;
+    private String title;
+    private String thumbnail;
+    private String description;
+    private LocalDateTime createdAt;
+    private boolean onlyApartUser;
+
+    @QueryProjection
+    public TogetherResponse(Long id,
+                            Category category,
+                            RecruitStatus recruitStatus,
+                            String createdBy,
+                            String title,
+                            String thumbnail,
+                            String description,
+                            LocalDateTime createdAt,
+                            boolean onlyApartUser) {
+        this.id = id;
+        this.category = category.getName();
+        this.recruitStatus = recruitStatus.getName();
+        this.createdBy = createdBy;
+        this.title = title;
+        this.thumbnail = thumbnail;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.onlyApartUser = onlyApartUser;
     }
 }
+
+
+//package kr.apartribebackend.article.dto.together;
+//
+//import kr.apartribebackend.article.domain.Together;
+//
+//import java.time.LocalDateTime;
+//
+//public record TogetherResponse(
+//        Long id,
+//        String category,
+//        String recruitStatus,
+//        String createdBy,
+//        String title,
+//        String thumbnail,
+//        String description,
+//        LocalDateTime createdAt){
+//    public static TogetherResponse from(Together together) {
+//        return new TogetherResponse(
+//                together.getId(),
+//                together.getCategory().getName(),
+//                together.getRecruitStatus().getName(),
+//                together.getCreatedBy(),
+//                together.getTitle(),
+//                together.getThumbnail(),
+//                together.getDescription(),
+//                together.getCreatedAt()
+//        );
+//    }
+//}
